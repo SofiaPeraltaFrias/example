5dd6c53e84271260a480e61f9c0b0d90246afb34
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-25 07:43:38 -0300

    Rewrite welcome to home page

--- resources/views/home.php
+++ resources/views/home.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Page</title>
+    <title>Home Page</title>
 </head>
 <body>
-    <h1>Contact Page</h1>
+    <h1>Home Page</h1>
 </body>
 </html>
