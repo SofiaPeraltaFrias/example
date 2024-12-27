23eba5f6ca5d757b591db42bbaef0380b62a8116
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-27 15:24:42 -0300

    Add 404 for non existing jobs

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -26,7 +26,11 @@ class Job {
   }
 
   public static function find(int $id): array {
-    return Arr::first(static::all(), fn($job) => $job['id'] == $id);
+    $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+    if (!$job) {
+      abort(404);
+    }
   }
 }
 
