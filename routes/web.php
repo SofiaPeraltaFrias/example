6d6ef0940d24090d9968ae2a1716c9f91ff7b15f
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-31 15:44:25 -0300

    Add Editing and Deleting of jobs

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,31 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+// JOBS
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->paginate(4);
     return view('jobs.index', ['jobs' => $jobs]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -38,6 +48,45 @@ Route::post('/jobs', function () {
     return redirect('/jobs');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
 });
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // Validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // Authorize (TODO)
+
+    // Update
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    // Redirect
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // Authorize (TODO)
+
+    // Delete
+    Job::findOrFail($id)->delete();
+
+    // Redirect
+    return redirect('/jobs');
+});
+
+
+
