7479fdce216c4cdf834043f08c900e4810aefed7
Sofia Peralta <sofiaperaltafrias@gmail.com>
2024-12-31 12:25:59 -0300

    Add validation to job create

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -15,8 +15,13 @@
                 <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                 <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="Shift Leader">
+                    <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="Shift Leader" required>
                   </div>
+                </div>
+                <div class="mt-1">
+                  @error('title')
+                    <p class="text-xs text-red-500 font-semibold">{{ $message }}</p>
+                  @enderror
                 </div>
               </div>
 
@@ -24,8 +29,13 @@
                 <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                 <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="$50,000">
+                    <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="$50,000" required>
                   </div>
+                </div>
+                <div class="mt-1">
+                  @error('salary')
+                    <p class="text-xs text-red-500 font-semibold">{{ $message }}</p>
+                  @enderror
                 </div>
               </div>
       
