296d5d7aa95687d5047a236d669f914a23175826
Sofia Peralta <sofiaperaltafrias@gmail.com>
2025-01-01 13:51:09 -0300

    Refactor routes

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-
-// JOBS
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
+Route::resource('jobs', JobController::class);
 
 
 
